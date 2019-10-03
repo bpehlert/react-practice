ae2371e4ffaf0855f9de4cb0293677457f816944
Benjamin Ehlert <benjamin.ehlert@gmail.com>
2019-10-03 17:32:51 -0600

    changed from class component to functional to use react hooks.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,77 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import SearchField from "./SearchField";
 import CardList from "./CardList";
 import { SWAPI } from "./config/keys";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      people: [],
-      searchField: ""
-    };
-  }
+const App = () => {
+  const [people, setPeople] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData = async () => {
+  const fetchData = (async () => {
     const res = await fetch(SWAPI);
     const json = await res.json();
-    this.setState({ people: json.results });
+    setPeople(json.results);
+  })();
+
+  const onSearchChange = e => {
+    setSearchField(e.target.value);
   };
 
-  onSearchChange = e => {
-    this.setState({
-      searchField: e.target.value
-    });
-  };
+  const filteredPeople = people.filter(person => {
+    return person.name.toLowerCase().includes(searchField.toLowerCase());
+  });
 
-  render() {
-    const { people, searchField } = this.state;
-
-    const filteredPeople = people.filter(person => {
-      return person.name.toLowerCase().includes(searchField.toLowerCase());
-    });
-
-    return (
-      <div>
-        <h1>Star Ward</h1>
-        <SearchField onSearchChange={this.onSearchChange} />
-        <CardList people={filteredPeople} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h1>Star Ward</h1>
+      <SearchField onSearchChange={onSearchChange} />
+      <CardList people={filteredPeople} />
+    </div>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//     constructor() {
+//       super();
+//       this.state = {
+//         people: [],
+//         searchField: ""
+//       };
+//     }
+
+//     componentDidMount() {
+//       this.fetchData();
+//     }
+
+//     fetchData = async () => {
+//       const res = await fetch(SWAPI);
+//       const json = await res.json();
+//       this.setState({ people: json.results });
+//     };
+
+//     onSearchChange = e => {
+//       this.setState({
+//         searchField: e.target.value
+//       });
+//     };
+
+//     render() {
+//       const { people, searchField } = this.state;
+
+//       const filteredPeople = people.filter(person => {
+//         return person.name.toLowerCase().includes(searchField.toLowerCase());
+//       });
+
+//       return (
+//         <div>
+//           <h1>Star Ward</h1>
+//           <SearchField onSearchChange={this.onSearchChange} />
+//           <CardList people={filteredPeople} />
+//         </div>
+//       );
+//     }
+//   }
+
+//   export default App;
