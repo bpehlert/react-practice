0b2d7f420080190dafe3eff5ef62d64e9aac66b9
bpehlert <benjamin.ehlert@gmail.com>
2019-11-07 21:43:49 -0700

    adjusted image size

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div>
+    <div className="loadingDiv">
       <img
         src="https://www.madhyamtech.com/assets/loader.gif"
         alt="loading_img"
